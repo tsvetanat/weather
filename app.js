000069e4c41b01a302ae72713058e4419939465e
tsvetanat <132619729+tsvetanat@users.noreply.github.com>
2023-07-31 11:36:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function showError(error){
 
 async function getWeather(latitude, longitude){
     try {
-        let api = ` https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
+        let api = `https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
         const response = await fetch(api);
         const data = await response.json();
         console.log(data);
